ddd67fb76f358b5b171361e76d96b1e6ae51ea6f
ZioTyr <ziotyr1@gmail.com>
2020-03-01 16:17:25 +0300

    Добавлены стили для комментариев

--- videoHosting/static/videoHosting/js/script.js
+++ videoHosting/static/videoHosting/js/script.js
@@ -21,7 +21,58 @@ $('.commentForm').on('submit', function (e) {
     let csrftoken = getCookie('csrftoken');
 
 
+    let Data = new Date();
+    let Year = Data.getFullYear();
+    let Month = Data.getMonth();
+    let Day = Data.getDate();
+    let Minutes = Data.getMinutes();
+    let Seconds = Data.getSeconds();
+
+// Преобразуем месяца
+    switch (Month) {
+        case 0:
+            fMonth = "января";
+            break;
+        case 1:
+            fMonth = "февраля";
+            break;
+        case 2:
+            fMonth = "марта";
+            break;
+        case 3:
+            fMonth = "апреля";
+            break;
+        case 4:
+            fMonth = "мае";
+            break;
+        case 5:
+            fMonth = "июня";
+            break;
+        case 6:
+            fMonth = "июля";
+            break;
+        case 7:
+            fMonth = "августа";
+            break;
+        case 8:
+            fMonth = "сентября";
+            break;
+        case 9:
+            fMonth = "октября";
+            break;
+        case 10:
+            fMonth = "ноября";
+            break;
+        case 11:
+            fMonth = "декабря";
+            break;
+    }
+
+    let dateTime = Day + " " + fMonth + " " + Year + "г." + " " + Minutes + ":" + Seconds;
+
+
     console.log("create post is working!");
+
     let $form = $(this);
     let $talk = $(this);
     $.ajax({
@@ -34,7 +85,7 @@ $('.commentForm').on('submit', function (e) {
         },
         success: function (json) {
             $form.find('.content_text').val('');
-            $talk.find('.talk').prepend("<li><strong>" + json.comment + "</strong> - <em> " + json.user + "</em> - <span> " + json.date + "</span></li>");
+            $talk.find('.talk').prepend("<li class='wrapper_comment comment_new'><div class='comment_user'>" + json.user + "</div> <div class='comment_text'> " + json.comment + "</div> <div class='comment_pub_date'> " + dateTime + "</div></li>");
             console.log("success");
         },
         error: function (xhr, errmsg, err) {
